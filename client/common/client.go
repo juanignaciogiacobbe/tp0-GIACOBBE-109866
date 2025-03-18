e9b1dc1f8535748eecc374701834c1aa7e6a5b6c
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2025-03-18 11:05:58 -0300

    ej5: deleting commented code on client

--- client/common/client.go
+++ client/common/client.go
@@ -124,39 +124,6 @@ func (c *Client) StartClientLoop() {
 
 	c.conn.Close()
 
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
-	// for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-	// 	// Create the connection the server in every loop iteration. Send an
-	// 	c.createClientSocket()
-
-	// 	// TODO: Modify the send to avoid short-write
-	// 	fmt.Fprintf(
-	// 		c.conn,
-	// 		"[CLIENT %v] Message N°%v\n",
-	// 		c.config.ID,
-	// 		msgID,
-	// 	)
-	// 	msg, err := bufio.NewReader(c.conn).ReadString('\n')
-	// 	c.conn.Close()
-
-	// 	if err != nil {
-	// 		log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-	// 			c.config.ID,
-	// 			err,
-	// 		)
-	// 		return
-	// 	}
-
-	// 	log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-	// 		c.config.ID,
-	// 		msg,
-	// 	)
-
-	// 	// Wait a time between sending one message and the next one
-	// 	time.Sleep(c.config.LoopPeriod)
-
-	// }
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
 
