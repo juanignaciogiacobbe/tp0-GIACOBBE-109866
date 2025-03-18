32debd6f2293bdc2501d81da3509ac25b7e27291
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2025-03-18 11:05:39 -0300

    ej5: client sends the bet to the server

--- client/common/client.go
+++ client/common/client.go
@@ -20,6 +20,12 @@ type ClientConfig struct {
 	ServerAddress string
 	LoopAmount    int
 	LoopPeriod    time.Duration
+
+	Nombre     string
+	Apellido   string
+	Documento  string
+	Nacimiento string
+	Numero     string
 }
 
 // Client Entity that encapsulates how
@@ -49,7 +55,43 @@ func (c *Client) createClientSocket() error {
 			err,
 		)
 	}
+
 	c.conn = conn
+	return nil
+}
+
+func (c *Client) sendMessage() error {
+	message := fmt.Sprintf("NOMBRE=%v|APELLIDO=%v|DOCUMENTO=%v|NACIMIENTO=%v|NUMERO=%v\n",
+		c.config.Nombre,
+		c.config.Apellido,
+		c.config.Documento,
+		c.config.Nacimiento,
+		c.config.Numero,
+	)
+	data := []byte(message)
+
+	totalWritten := 0
+	messageLen := len(data)
+
+	for totalWritten < messageLen {
+		n, err := c.conn.Write(data[totalWritten:])
+		if err != nil {
+			log.Errorf("action: send_bet | result: fail | client_id: %v | error: %v", c.config.ID, err)
+			return err
+		}
+		totalWritten += n
+	}
+
+	msg, err := bufio.NewReader(c.conn).ReadString('\n')
+
+	if err != nil {
+		log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v", c.config.ID, err)
+		return err
+	}
+
+	log.Infof("action: send_bet | result: success | dni: %v | numero: %v", c.config.Documento, c.config.Numero)
+
+	log.Infof("action: receive_message | result: success | client_id: %v | msg: %v", c.config.ID, msg)
 	return nil
 }
 
@@ -64,39 +106,57 @@ func (c *Client) StartClientLoop() {
 		os.Exit(0)
 	}()
 
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
+	err := c.createClientSocket()
+	if err != nil {
+		log.Errorf("action: connect | result: fail | client_id: %v | error: %v", c.config.ID, err)
+		return
+	}
+
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
-
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
-
+		err := c.sendMessage()
 		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
-			return
+			log.Errorf("action: send_bet | result: fail | client_id: %v | error: %v", c.config.ID, err)
+			continue
 		}
 
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		// Wait a time between sending one message and the next one
 		time.Sleep(c.config.LoopPeriod)
-
 	}
+
+	c.conn.Close()
+
+	// There is an autoincremental msgID to identify every message sent
+	// Messages if the message amount threshold has not been surpassed
+	// for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
+	// 	// Create the connection the server in every loop iteration. Send an
+	// 	c.createClientSocket()
+
+	// 	// TODO: Modify the send to avoid short-write
+	// 	fmt.Fprintf(
+	// 		c.conn,
+	// 		"[CLIENT %v] Message N°%v\n",
+	// 		c.config.ID,
+	// 		msgID,
+	// 	)
+	// 	msg, err := bufio.NewReader(c.conn).ReadString('\n')
+	// 	c.conn.Close()
+
+	// 	if err != nil {
+	// 		log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+	// 			c.config.ID,
+	// 			err,
+	// 		)
+	// 		return
+	// 	}
+
+	// 	log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
+	// 		c.config.ID,
+	// 		msg,
+	// 	)
+
+	// 	// Wait a time between sending one message and the next one
+	// 	time.Sleep(c.config.LoopPeriod)
+
+	// }
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
 
