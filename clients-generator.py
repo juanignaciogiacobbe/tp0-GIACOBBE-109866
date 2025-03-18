0dacb116621bd9680d817734c38e0da37d120e66
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2025-03-18 11:04:28 -0300

    ej5: modifying client generator script to receive bet env variables

--- clients-generator.py
+++ clients-generator.py
@@ -27,6 +27,14 @@ compose_content += "      - testing_net\n\n"
 compose_content += "    volumes:\n"
 compose_content += "      - ./server/config.ini:/server/config.ini\n\n"
 
+
+# Client env variables
+name = "Santiago Lionel"
+last_name = "Lorca"
+dni = "30904465"
+birth_date = "1999-03-17"
+number = "7574"
+
 for i in range(1, clients_num + 1):
     compose_content += f"  client{i}:\n"
     compose_content += f"    container_name: client{i}\n"
@@ -35,6 +43,11 @@ for i in range(1, clients_num + 1):
     compose_content += "    environment:\n"
     compose_content += f"      - CLI_ID={i}\n"
     # compose_content += "      - CLI_LOG_LEVEL=DEBUG\n"
+    compose_content += f"      - NOMBRE={name}\n"
+    compose_content += f"      - APELLIDO={last_name}\n"
+    compose_content += f"      - DOCUMENTO={dni}\n"
+    compose_content += f"      - NACIMIENTO={birth_date}\n"
+    compose_content += f"      - NUMERO={number}\n"
     compose_content += "    networks:\n"
     compose_content += "      - testing_net\n"
     compose_content += "    depends_on:\n"
