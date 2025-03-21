65c425f45e3a7075956eb6c4170a1e21f03334b9
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2025-03-21 20:06:41 -0300

    ej6: modifying the server to process batches(or chunks). The server handles multiple bets inside a client packet

--- server/common/server.py
+++ server/common/server.py
@@ -24,7 +24,7 @@ class Server:
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
         self._client_sockets = []
-        self._server_socket.settimeout(1)
+        self._server_socket.settimeout(0.1)
 
         # Handle SIGTERM signal
         signal.signal(signal.SIGTERM, self.handle_signal)
@@ -46,54 +46,89 @@ class Server:
             
                 
             self._client_sockets.append(client_sock)
-            threading.Thread(target=self.__handle_client_connection, args=(client_sock,)).start()
-
+            # threading.Thread(target=self.__handle_client_connection, args=(client_sock,)).start()
+            self.__handle_client_connection(client_sock)
+    
     def __handle_client_connection(self, client_sock):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
-        try:
-            bets = []
-            bet = self.__receive_message(client_sock)
-
-            if bet:
-                addr = client_sock.getpeername()
-                bets.append(bet)
-                logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {bet}')
-
-            if bets:
-                store_bets(bets)
-                logging.info(f'action: apuesta_almacenada | result: success | dni: {bet.document} | numero: {bet.number}')
-
-        except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
-        finally:
-            client_sock.close()
-
-    def __receive_message(self, client_sock):
-        bet_fields = ['agency', 'first_name', 'last_name', 'document', 'birthdate', 'number']
-        bet_values = {}
         buffer = b''
+        read = 0
+        processed_batch_size = False
+        batch_size = 0
+        total_bets_received = 0
 
-        while bet_fields:
-            data = client_sock.recv(1024)
-            buffer += data
-
-            while len(buffer) >= U8_SIZE:
-                len_data = int.from_bytes(buffer[:U8_SIZE], byteorder='big')
-                buffer = buffer[U8_SIZE:]
-
-                if len(buffer) < len_data:
+        while True:
+            try:
+                data = client_sock.recv(8192 - len(buffer))
+                if not data:
+                    logging.info("action: receive_message | result: finished connection")
                     break
+            except (BrokenPipeError, TimeoutError) as e:
+                logging.info("action: receive_message | result: finished connection | error: %s", e)
+                break 
+            
+            buffer += data
+            read += len(data)
 
-                field_data, buffer = buffer[:len_data], buffer[len_data:]
-                field_name = bet_fields.pop(0) 
-                bet_values[field_name] = field_data.decode('utf-8')
+            # Process batch size if not done already
+            if not processed_batch_size and len(buffer) >= 16:
+                batch_size = int.from_bytes(buffer[:16], byteorder='big')
+                buffer = buffer[16:]
+                read -= 16
+                processed_batch_size = True
+
+            # Wait until the whole batch is received
+            if read < batch_size:
+                continue
+
+            # process chunk
+            try:
+                batch, buffer = self.parse_batch(buffer, batch_size)
+                total_bets_received += len(batch)
+                store_bets(batch)
+
+                logging.info("action: apuesta_recibida | result: success | cantidad: %d", len(batch))
+
+                ok = 1
+                client_sock.sendall(ok.to_bytes(1, byteorder='big'))
+            except Exception as e:
+                logging.error("action: apuesta_recibida | result: fail | cantidad: %d | error: %s", len(batch), e)
+                try:
+                    error = 0
+                    client_sock.sendall(error.to_bytes(1, byteorder='big'))
+                except BrokenPipeError:
+                    logging.error("action: send_message | result: finished connection")
+                    return
+            
+            processed_batch_size = False
+            read = 0
         
-        return Bet(**bet_values)
+        logging.info("action: connection_finished | result: success | total_bets_received: %d", total_bets_received)
+
+    def parse_batch(self, buffer, batch_size):
+        batch = []
+
+        while batch_size > 0:
+            bet_fields = ['agency', 'first_name', 'last_name', 'document', 'birthdate', 'number']
+            bet_values = {}
+
+            while bet_fields:
+                if len(buffer) >= 8:
+                    len_data = int.from_bytes(buffer[:U8_SIZE], byteorder='big')
+                    buffer = buffer[U8_SIZE:]
+                    batch_size -= 8
+
+                    field_data, buffer = buffer[:len_data], buffer[len_data:]
+                    field_name = bet_fields.pop(0) 
+                    bet_values[field_name] = field_data.decode('utf-8')
+
+            try:
+                batch.append(Bet(**bet_values))
+            except TypeError as e:
+                logging.info("action: apuesta_recibida | result: fail | cantidad: %d", len(batch))
+                return e
+            
+        logging.info("action: apuesta_recibida | result: success | cantidad: %d", len(batch))
+        return batch, buffer
 
 
     def handle_signal(self, signum, frame):
